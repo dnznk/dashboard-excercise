f085742e1192027eb7cc996af42432151b9ed23e
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2023-01-11 13:04:17 +0100

    Updated Calendar component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -8,19 +8,22 @@
     <div class="cal-date">
       <h2>12</h2>
     </div>
-    <div class="first entry">
-      <p class="time">12:00</p>
-      <p class="definition">Zoom Meeting</p>
-    </div>
-    <div class="second entry">
-      <p class="time">14:00</p>
-      <p class="definition">Lunch</p>
+    <div class="entries">
+      <div class="first entry">
+        <p class="time">12:00</p>
+        <p class="definition">Zoom Meeting</p>
+      </div>
+      <div class="second entry">
+        <p class="time">14:00</p>
+        <p class="definition">Lunch</p>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
 .cal {
+  position: relative;
   width: 300px;
   height: 300px;
   display: grid;
@@ -29,5 +32,45 @@
   border: 1px solid black;
   border-radius: 8px;
   margin: auto;
+
+  grid-template-areas:
+    "box1 box2"
+    "box3 box3"
+    "box3 box3";
+
+  grid-template-rows: 1fr 3fr;
+  grid-template-columns: 1fr 2fr;
+}
+
+.cal-heading {
+  grid-area: box1;
+  font-size: 1rem;
+  color: red;
+}
+
+.cal-date {
+  font-size: 7rem;
+  color: red;
+  opacity: 0.2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  right: 1rem;
+  bottom: 1rem;
+  z-index: -1;
+}
+
+.entries {
+  grid-area: box3;
+}
+
+.time {
+  font-size: 1rem;
+  color: gray;
+}
+
+.definition {
+  font-size: 1.55rem;
 }
 </style>
