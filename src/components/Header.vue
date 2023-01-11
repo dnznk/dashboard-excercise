73f2c776446db9f0cb906335c481727979abfcb1
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2023-01-11 21:46:41 +0100

    Cleaned up and added flexible greeting text

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,27 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      greeting: "",
+    };
+  },
+  created() {
+    let currentHour = new Date().getHours();
+    if (currentHour < 12) {
+      this.greeting = "Good morning!";
+    } else if (currentHour < 18) {
+      this.greeting = "Good afternoon!";
+    } else {
+      this.greeting = "Good evening!";
+    }
+  },
+};
+</script>
 
 <template>
   <div class="greeting-container">
     <div class="profile-img"><img src="../assets/profile.png" alt="" /></div>
-    <div class="greeting">Good morning!</div>
+    <div class="greeting">{{ greeting }}</div>
   </div>
 </template>
 
