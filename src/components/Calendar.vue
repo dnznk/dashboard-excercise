857fa33e28d990846fb9131ed1fd2f1d9d31f398
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2023-01-11 12:36:44 +0100

    First Design implementation and first Component

---
+++ src/components/Calendar.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+
+<template>
+  <div class="cal">
+    <div class="cal-heading">
+      <h1>Calendar</h1>
+    </div>
+    <div class="cal-date">
+      <h2>12</h2>
+    </div>
+    <div class="first entry">
+      <p class="time">12:00</p>
+      <p class="definition">Zoom Meeting</p>
+    </div>
+    <div class="second entry">
+      <p class="time">14:00</p>
+      <p class="definition">Lunch</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.cal {
+  width: 300px;
+  height: 300px;
+  display: grid;
+  text-align: left;
+  padding: 1rem;
+  border: 1px solid black;
+  border-radius: 8px;
+  margin: auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <div class="greeting-container">
+    <div class="profile-img"><img src="../assets/profile.png" alt="" /></div>
+    <div class="greeting">Good morning!</div>
+  </div>
+</template>
+
+<style>
+.greeting-container {
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  place-items: center;
+  padding: 1rem;
+}
+
+.profile-img img {
+  width: 10vmin;
+  height: 10vmin;
+  border-radius: 50%;
+}
+
+.greeting {
+  font-size: clamp(1rem, 5vmin, 5rem);
+  color: black;
+}
+</style>
