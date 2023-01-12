dad62adb72be821f6a459e76ce216afe6637ff40
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2023-01-12 20:09:55 +0100

    Implementation and first improvements for Maps and Animation vue components

---
+++ src/components/Animation.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="animation-container box">
+    <img id="rocket" src="../assets/rocket.png" alt="" />
+  </div>
+</template>
+<script>
+// export default {
+//   data() {
+//     return {
+//       isActive: false,
+//     };
+//   },
+// };
+</script>
+<style>
+.animation-container {
+  background-image: radial-gradient(yellow 2%, transparent 2%),
+    radial-gradient(yellow 3%, transparent 5%);
+  background-color: black;
+  background-position: 0 0, 50px 50px;
+  background-size: 100px 100px;
+}
+
+.button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+.button button {
+  width: fit-content;
+  background-color: hotpink;
+  color: black;
+  padding: 0.75rem 1rem;
+}
+
+#rocket {
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  animation: moveRocket 8s linear 500ms infinite;
+  transform: rotate(90deg);
+  animation-play-state: running;
+}
+
+@keyframes moveRocket {
+  0% {
+    bottom: 5px;
+    left: 5px;
+    transform: rotate(90deg);
+  }
+  25% {
+    bottom: 5px;
+    left: 231px;
+    transform: rotate(0deg);
+  }
+  50% {
+    bottom: 231px;
+    left: 231px;
+    transform: rotate(-90deg);
+  }
+  75% {
+    bottom: 231px;
+    left: 5px;
+    transform: rotate(-180deg);
+  }
+  100% {
+    bottom: 5px;
+    left: 5px;
+    transform: rotate(-270deg);
+  }
+}
+</style>
---
+++ src/components/Maps.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+  name: "OpenStreetMap",
+  data() {
+    return {
+      address: "Space Squad 🚀",
+    };
+  },
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      var map = L.map("map").setView([52.4683, 13.38981], 15);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      var marker = L.marker([52.4683, 13.38981]).addTo(map);
+      marker.bindPopup(this.address).openPopup();
+    },
+  },
+};
+</script>
+
+<style>
+#map {
+  width: 300px;
+  height: 300px;
+}
+</style>
