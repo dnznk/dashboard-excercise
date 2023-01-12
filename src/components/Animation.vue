842dff082b44c913b285ff98f88b14d2696b4297
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2023-01-12 20:52:29 +0100

    Added a button element aswell as functionality to change animation style property to play and pause the animation

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="animation-container box">
+    <button id="play-pause-button" @click="toggleAnimation">
+      ENGINE ON/OFF
+    </button>
     <img id="rocket" src="../assets/rocket.png" alt="" />
   </div>
 </template>
 <script>
-// export default {
-//   data() {
-//     return {
-//       isActive: false,
-//     };
-//   },
-// };
+export default {
+  methods: {
+    toggleAnimation() {
+      let rocket = document.getElementById("rocket");
+      if (rocket.style.animationPlayState === "running") {
+        rocket.style.animationPlayState = "paused";
+      } else {
+        rocket.style.animationPlayState = "running";
+      }
+    },
+  },
+};
 </script>
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono&display=swap");
+
 .animation-container {
   background-image: radial-gradient(yellow 2%, transparent 2%),
     radial-gradient(yellow 3%, transparent 5%);
@@ -28,7 +38,7 @@
 }
 .button button {
   width: fit-content;
-  background-color: hotpink;
+  background-color: rgb(255, 105, 180);
   color: black;
   padding: 0.75rem 1rem;
 }
@@ -39,7 +49,25 @@
   left: 0px;
   animation: moveRocket 8s linear 500ms infinite;
   transform: rotate(90deg);
-  animation-play-state: running;
+  animation-play-state: paused;
+}
+
+#play-pause-button {
+  position: absolute;
+  width: fit-content;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* background-color: hotpink; */
+  background: rgba(255, 105, 180, 0.9);
+  border: 0;
+  border-radius: 10px;
+  padding: 0.5rem 1rem;
+  color: purple;
+  font-size: 1rem;
+  mix-blend-mode: screen;
+  color: black;
+  font-family: "Space Mono", monospace;
 }
 
 @keyframes moveRocket {
