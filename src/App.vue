0641efbead79272f576acd682b16779ecf5b4530
Dan Zenke <107815714+dnznk@users.noreply.github.com>
2023-01-12 13:51:15 +0100

    Added styling for each box

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from "./components/Header.vue";
 import Calendar from "./components/Calendar.vue";
 import Weather from "./components/Weather.vue";
 import Unsplash from "./components/Unsplash.vue";
+import News from "./components/News.vue";
 </script>
 
 <template>
@@ -10,6 +11,15 @@ import Unsplash from "./components/Unsplash.vue";
   <Calendar />
   <Weather />
   <Unsplash />
+  <News />
 </template>
 
-<style scoped></style>
+<style scoped>
+.box {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  box-shadow: 4.5px 4.5px 25px #dddedb, -4.5px -4.5px 25px #ffffff;
+}
+</style>
